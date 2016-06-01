3ea931fa425bf0f891d3a651f960e3c5d7708ab2
Luz <luzma_87@yahoo.com>
2016-06-01 11:40:08 -0500

    +jreyes33 [KFE-000] deleting run e2e pipeline step

--- pipeline/04-run_e2e_pipeline.sh
+++
@@ -1,2 +0,0 @@
-***REMOVED***
-curl -u vrokida:muplE9Ctid7PCWnxVzZjR4zUsCnXiNKHIGSCvWwlm-8 -X POST -H 'Accept: application/vnd.snap-ci.com.v1+json' -H 'Content-type: application/json' https://api.snap-ci.com/project/Kushki/e2e-tests/branch/master/trigger
