96dfcc47a13889865eab96f7a40c89498d7a68fd
Luz <luzma_87@yahoo.com>
2016-07-12 14:31:15 -0500

    +@jreyes33 [KFE-106] tagging in git hub when releasing

--- pipeline/03-distribute.sh
+++ pipeline/03-distribute.sh
@@ -1,4 +1,12 @@
 ***REMOVED***
+
+set -e
+
+artifact_version=$(gradle --quiet printVersion)
+
+git tag --annotate "v$artifact_version" -m "Release for version $artifact_version"
+git push --tags
+
 gradle clean bintrayUpload
 
 # ENVIRONMENT VARIABLES:
