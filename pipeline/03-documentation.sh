7eeb664558812db726e4b3d337fb387a90ec9a96
Jonathan Reyes <j@jreyes.org>
2016-08-29 11:42:08 -0500

    [KFE-180] Add pipeline script to publish docs

---
+++ pipeline/03-documentation.sh
@@ -0,0 +1,16 @@
+***REMOVED***
+
+set -e
+
+artifact_version=$(gradle --quiet printVersion)
+target_dir="docs/$artifact_version"
+
+gradle clean javadoc
+git checkout gh-pages
+git pull origin gh-pages
+mkdir -p "$target_dir"
+cp -r build/docs/javadoc/* "$target_dir"
+git add "$target_dir"
+git commit -m "Update docs for v$artifact_version"
+git push origin gh-pages
+echo "Documentation published to https://kushki.github.io/kushki-java/docs/$artifact_version/"
