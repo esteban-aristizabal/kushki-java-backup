33682b16463107c7a373d46944ce61a6fa227f98
Victor <vandresmartinez@gmail.com>
2017-10-09 01:41:09 -0500

    KV-2018 add env vars

--- pipeline/04-distribute.sh
+++ pipeline/04-distribute.sh
@@ -4,8 +4,8 @@ set -e
 
 artifact_version=$(gradle --quiet printVersion)
 
-git tag --annotate "v$artifact_version" -m "Release for version $artifact_version"
-git push --tags
+#git tag --annotate "v$artifact_version" -m "Release for version $artifact_version"
+#git push --tags
 
 gradle clean bintrayUpload
 
