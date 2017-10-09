39f04a486e79d312e25c10f9f45393cf9e32e72f
Victor <vandresmartinez@gmail.com>
2017-10-09 01:45:12 -0500

    KV-2018 add env vars

--- pipeline/04-distribute.sh
+++ pipeline/04-distribute.sh
@@ -6,8 +6,11 @@ artifact_version=$(gradle --quiet printVersion)
 
 #git tag --annotate "v$artifact_version" -m "Release for version $artifact_version"
 #git push --tags
+#CI_BRANCH
 
+if [ "$CI_BRANCH" == "DEVELOPE" ]; then
 gradle clean bintrayUpload
+***REMOVED***
 
 # ENVIRONMENT VARIABLES:
 # STAGE
