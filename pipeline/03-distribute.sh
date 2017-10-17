5970bfaca4fb934d625db4002372389a33a85723
Victor Martinez <vandresmartinez@gmail.com>
2017-10-17 12:56:02 -0500

    fix branch name

--- pipeline/03-distribute.sh
+++ pipeline/03-distribute.sh
@@ -2,11 +2,11 @@
 
 # Waking up herokuapp
 curl $BACKOFFICE_URL > /dev/null
-if [ "$CI_BRANCH" == "MASTER" ]; then
+if [ "$CI_BRANCH" == "master" ]; then
 gradle clean bintrayUpload
 ***REMOVED***
 # ARTIFACTS:
 # ARTIFACT build/libs
 #
 # ENVIRONMENT VARIABLES:
-#   BACKOFFICE_URL https://backof***REMOVED***ce-qa.kushkipagos.com/
+#   BACKOFFICE_URL https://backof***REMOVED***ce   -qa.kushkipagos.com/
